59cea550d6bb7c4196d3611fdbe338d510edec56
BaoAL31 <jemboong@gmail.com>
2025-04-30 10:21:12 +1000

    added Login Servlet

--- demo/src/main/webapp/WEB-INF/classes/controller/LoginServlet.java
+++ demo/src/main/webapp/WEB-INF/classes/controller/LoginServlet.java
@@ -31,43 +31,35 @@ public class LoginServlet extends HttpServlet {
         if (manager == null) {
             throw new ServletException("DBManager not found in session");
         }
-
         User user = null;
 
         try {
-
             user = manager.findUser(email, password);
-
         } catch (SQLException ex) {
-
             Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
-
         }
 
         if (!validator.validateEmail(email)) {
-
             // 8-set incorrect email error to the session
-
+            session.setAttribute("errorMsg", "Invalid email format.");
             // 9- redirect user back to the login.jsp
-
+            request.getRequestDispatcher("login.jsp").include(request, response);
         } else if (!validator.validatePassword(password)) {
-
             // 11-set incorrect password error to the session
-
+            session.setAttribute("errorMsg", "Invalid password format.");
             // 12- redirect user back to the login.jsp
-
+            response.sendRedirect("login.jsp");
         } else if (user != null) {
-
             // 13-save the logged in user object to the session
-
+            session.setAttribute("user", user);
             // 14- redirect user to the main.jsp
-
+            request.getRequestDispatcher("landing_page.jsp").include(request, response);
         } else {
-
             // 15-set user does not exist error to the session
-
+            session.setAttribute("errorMsg", "User does not exist.");
             // 16- redirect user back to the login.jsp
-
+            request.getRequestDispatcher("login.jsp").include(request, response);
         }
+        
     }
 }
