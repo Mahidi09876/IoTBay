59acc109fd077f00e74579d1b207a5e906f5473e
BaoAL31 <jemboong@gmail.com>
2025-05-14 10:23:04 +1000

    Updated DeviceDao

--- demo/src/main/java/model/dao/DeviceDAO.java
+++ demo/src/main/java/model/dao/DeviceDAO.java
@@ -11,17 +11,6 @@ public class DeviceDAO {
         this.connection = connection;
     }
 
-<<<<<<< HEAD
-    // Create a new device (Staff only)
-    public void addDevice(Device device) throws SQLException {
-        String query = "INSERT INTO device (device_name, type, unit_price, stock) VALUES (?, ?, ?, ?)";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, device.getName());
-            statement.setString(2, device.getType());
-            statement.setDouble(3, device.getPrice());
-            statement.setInt(4, device.getStock());
-            statement.executeUpdate();
-=======
     public Device getDeviceById(int id) throws SQLException {
         String query = "SELECT * FROM device WHERE device_id = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
@@ -36,59 +25,10 @@ public class DeviceDAO {
                             resultSet.getInt("stock"));
                 }
             }
->>>>>>> main
         }
+        return null; // Return null if no device is found
     }
 
-<<<<<<< HEAD
-    // Read all devices (Accessible to both staff and customers)
-    public List<Device> getAllDevices() throws SQLException {
-        List<Device> devices = new ArrayList<>();
-        String query = "SELECT * FROM device";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                devices.add(new Device(
-                        rs.getInt("device_id"),
-                        rs.getString("device_name"),
-                        rs.getString("type"),
-                        rs.getDouble("unit_price"),
-                        rs.getInt("stock")));
-            }
-        }
-        return devices;
-    }
-
-    // Search devices by name and type (Accessible to both staff and customers)
-    public List<Device> searchDevices(String name, String type) throws SQLException {
-        List<Device> devices = new ArrayList<>();
-        String query = "SELECT * FROM device WHERE device_name LIKE ? AND type LIKE ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, "%" + name + "%");
-            statement.setString(2, "%" + type + "%");
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                devices.add(new Device(
-                        rs.getInt("device_id"),
-                        rs.getString("device_name"),
-                        rs.getString("type"),
-                        rs.getDouble("unit_price"),
-                        rs.getInt("stock")));
-            }
-        }
-        return devices;
-    }
-
-    // Update a device (Staff only)
-    public void updateDevice(Device device) throws SQLException {
-        String query = "UPDATE device SET device_name = ?, type = ?, unit_price = ?, stock = ? WHERE device_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, device.getName());
-            statement.setString(2, device.getType());
-            statement.setDouble(3, device.getPrice());
-            statement.setInt(4, device.getStock());
-            statement.setInt(5, device.getId());
-=======
     public int getStock(int deviceId) throws SQLException {
         String query = "SELECT stock FROM Device WHERE device_id = ?";
         try (PreparedStatement ps = connection.prepareStatement(query)) {
@@ -107,20 +47,8 @@ public class DeviceDAO {
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, newStock);
             statement.setInt(2, deviceId);
->>>>>>> main
             statement.executeUpdate();
         }
     }
 
-<<<<<<< HEAD
-    // Delete a device (Staff only)
-    public void deleteDevice(int id) throws SQLException {
-        String query = "DELETE FROM device WHERE device_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, id);
-            statement.executeUpdate();
-        }
-    }
-=======
->>>>>>> main
 }
